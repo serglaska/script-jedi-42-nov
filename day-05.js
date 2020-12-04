edff01e5b1ecce69d6fce8b2d7ec9bafe8747ab7
serglaska <44259501+serglaska@users.noreply.github.com>
2020-12-04 21:20:37 +0200

    Create day-05.js

---
+++ day-05.js
@@ -0,0 +1,15 @@
+// task https://www.codewars.com/kata/is-this-my-tail/train/javascript
+
+function correctTail(body, tail) {
+  const sub = body.substr(body.length - (tail.length))
+  if (sub === tail) return true;
+  else return false;
+};
+
+// task https://www.codewars.com/kata/56f6ad906b88de513f000d96
+
+const bonusTime = (salary, bonus) => bonus ? `£${salary*10}` : `£${salary}`;
+
+// task https://www.codewars.com/kata/5a58d889880385c2f40000aa
+
+const automorphic = (n) => String(n * n).endsWith(n) ? 'Automorphic' : 'Not!!';
