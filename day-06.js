1662a20ded404de9b2e6671f24aeb5a2f57f754b
serglaska <44259501+serglaska@users.noreply.github.com>
2020-12-05 19:56:58 +0200

    Create day-06.js

---
+++ day-06.js
@@ -0,0 +1,11 @@
+// task https://www.codewars.com/kata/maximum-multiple/train/javascript
+
+const maxMultiple = (divisor, bound) => Math.floor(bound / divisor) * divisor;
+
+// task https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+
+const hero = (bullets, dragons) => dragons * 2 <= bullets ? true : false;
+
+// task  https://www.codewars.com/kata/59441520102eaa25260000bf
+
+const unusualFive = () => 'five?'.length;
