aa7ecede97ae0d3a40dabd140f305e51a46cb201
serglaska <44259501+serglaska@users.noreply.github.com>
2020-12-07 23:38:32 +0200

    Create day-08.js

---
+++ day-08.js
@@ -0,0 +1,27 @@
+// task http://www.codewars.com/kata/5722fd3ab7162a3a4500031f
+
+const whatNumberIsIt = (n) => {
+  switch (n) {
+    case 0: 'Input number is 0';
+    case Infinity: return 'Input number is Number.POSITIVE_INFINITY';
+    case Number.MIN_VALUE: return 'Input number is Number.MIN_VALUE';
+    case Number.MAX_VALUE: return 'Input number is Number.MAX_VALUE';
+    case Number.NEGATIVE_INFINITY: return 'Input number is Number.NEGATIVE_INFINITY';
+    default: return !!n ? `Input number is ${n}`: 'Input number is Number.NaN';
+  };
+};
+
+// task https://www.codewars.com/kata/57238ceaef9008adc7000603
+
+const colorOf = (...rgb) => {
+  const newArr = rgb.map(el => {
+    let trick = el.toString(16);
+    return trick.length === 1 ? '0' + trick : trick;
+  });
+  
+  return `#${newArr.join('')}`;
+};
+
+// http://www.codewars.com/kata/57256064856584bc47000611
+
+const howManySmaller = (arr,n) => arr.filter(el => +el.toFixed(2) < n).length;
