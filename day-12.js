2201bb86db9cdae9d9d1bac038dfe441f2f40ecd
Sergey <sergii.pietinov@exsportia.com>
2020-12-12 22:09:22 +0200

    serik->fixed: mistakes

--- day-12.js
+++ day-12.js
@@ -2,17 +2,12 @@
 
 const areEqual = (s1, s2) => {
   if (s1.size !== s2.size) return false;
-  s1.forEach((item) => {
-    if (!s2.has(item)) {
+  for (const value of s1) {
+    if (!s2.has(value)) {
       return false;
     }
-  });
+  }
   return true;
 };
 
-const notEqual = (s1, s2) => !!areEqual(s1, s2);
-const s1 = new Set([1, 2, 3]);
-const s2 = new Set([1, 2, 5]);
-
-console.log("🚀 ~ file: day-12.js ~ line 14 ~ notEqual", notEqual(s1, s2))
-
+const notEqual = (s1, s2) => !areEqual(s1, s2);
